22124989334c98a6c9cd7a681b23289c9f79dca1
adamjorycromer <adamjorycromer@gmail.com>
2018-12-19 19:52:18 -0700

    don't know

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,4 +6,28 @@ $(document).ready(function () {
         event.preventDefault();
     });
 
+
+
+
+    function searchVolAPI() {
+
+        var apiKey = "e356bef55bb04a799eef5b90f55f3d94";
+        var queryURL = "https://apidata.guidestar.org/charitycheck/v1/54-1774039?Subscription-Key=" + apiKey + "&search_term=saltlakecity";
+
+        $.ajax({
+            POST: "https://apidata.guidestar.org/charitycheck/v1/54-1774039",
+            method: "GET",
+            Host: "apidata.guidestar.org",
+            Subscription-Key: apiKey
+        }).then(function (response) {
+
+            console.log(response);
+
+
+
+        });
+    }
+
+    searchVolAPI();
+
 });
