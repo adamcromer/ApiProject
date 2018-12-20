3a3541101209755f77714b1fba886da85aa86095
Jeremy Atwood <you@example.com>
2018-12-19 19:29:34 -0700

    added the location data into the map so it will display the location data.

--- assets/javascript/atwood.js
+++ assets/javascript/atwood.js
@@ -23,6 +23,25 @@ $(document).ready(function () {
             });
 
         });
+        var infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+
+        service.getDetails({
+            placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+        }, function (place, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: place.geometry.location
+                });
+                google.maps.event.addListener(marker, 'click', function () {
+                    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                        'Place ID: ' + place.place_id + '<br>' +
+                        place.formatted_address + '</div>');
+                    infowindow.open(map, this);
+                });
+            }
+        });
 
 
         // creating the marker variable to control the cluster of markers.
