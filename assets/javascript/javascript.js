4588aa27892e939826cf51bc961a0f0a41eb8228
adamjorycromer <adamjorycromer@gmail.com>
2018-12-21 15:37:13 -0700

    added jeffsum and calendar actually shows on page

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,37 @@
 $(document).ready(function () {
 
     var submit = $("#submit");
+    var calendar = $("#calendar").fullCalendar("getCalendar");
+    var currentTime = $("#currentTime");
+    var emptyTimeVar;
+
+    function setCurrentTime() {
+        currentTime.text(moment().format('LLLL'));
+    }
+
+    
 
     submit.click(function (event) {
         event.preventDefault();
     });
 
-    
+    function loadCalendar() {
+
+        $("#calendar").fullCalendar({
+
+        });
+
+
+    }
+
+    // calendar.fullCalendar({
+    //     dayClick: function () {
+    //         var dayDiv = $("<div>");
+    //         var dayEvent = 
+    //     }
+    // })
+
+
+    loadCalendar();
 
 });
