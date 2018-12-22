fbc60fe6a772079f7184d3e384d59995257d3384
adamjorycromer <adamjorycromer@gmail.com>
2018-12-21 21:35:25 -0700

    added fonts and calendar

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,37 +1,50 @@
 $(document).ready(function () {
 
     var submit = $("#submit");
-    var calendar = $("#calendar").fullCalendar("getCalendar");
+    var calendar = $('#calendar');
     var currentTime = $("#currentTime");
     var emptyTimeVar;
+    var next = $("#next");
+    var previous = $("#previous");
+    var month = $("#monthView");
+    var week = $("#weekView");
+    var day = $("#dayView");
 
+    //Function to show the current time
     function setCurrentTime() {
-        currentTime.text(moment().format('LLLL'));
+        currentTime.text(moment().format('dddd, MMMM Do, YYYY [at] h:mm:ss A'));
     }
-
-    
+    //Starts the time function to run every second
+    emptyTimeVar = setInterval(setCurrentTime, 1000);
 
     submit.click(function (event) {
         event.preventDefault();
     });
 
     function loadCalendar() {
-
-        $("#calendar").fullCalendar({
-
+        calendar.fullCalendar({
         });
-
-
     }
 
-    // calendar.fullCalendar({
-    //     dayClick: function () {
-    //         var dayDiv = $("<div>");
-    //         var dayEvent = 
-    //     }
-    // })
+    next.click(function() {
+        calendar.fullCalendar('next');
+    });
 
+    previous.click(function() {
+        calendar.fullCalendar('prev');
+    });
 
+    month.click(function() {
+        calendar.fullCalendar('changeView', 'agendaMonth');
+    });
+
+    week.click(function() {
+        calendar.fullCalendar('changeView', 'agendaWeek');
+    });
+
+    day.click(function() {
+        calendar.fullCalendar('changeView', 'agendaDay');
+    });
     loadCalendar();
 
 });
